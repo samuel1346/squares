602c0105c21b285c24ced9a37764224a38b5a617
samuel1346 <samielnsilva@gmail.com>
2024-09-23 00:50:25 -0300

    still tryng

--- src/app/components/navigationComponents/NavigationComponent.jsx
+++ src/app/components/navigationComponents/NavigationComponent.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import Link from 'next/link';
-export const dynamic = "force-dynamic";
 
 const NavigationComponent = async () => {
     const uniqueCategories = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/API/products/categories`, {
       next: { revalidate: 0 }
-    }).then(r=>r.json()).catch(e=>console.log(e,'error'))
+    }).then(r=>r.json()).catch(e=>{console.log(e,'error')
+      return []})
 
 
     return (
       <div>
         <ul className='flex flex-row flex-wrap'>
-          {uniqueCategories.map((category, index) => (
+          {uniqueCategories?.map((category, index) => (
             <li key={index} className='rounded-lg border-2 border-zinc-600 bg-zinc-900/75 m-2 p-2  text-zinc-200'>
               <Link href={`/categories/${category}`}>
                 {category}
